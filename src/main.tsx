3308e342210d2c5553b7a2f7b81b29639e607109
홍요한 <ghddygks45@gmail.com>
2026-01-30 00:00:11 +0900

    fix: tailwind v4 및 svg 설정 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { router } from "@/app/router";
-import "./index.css";
+import "./global.css";
 
 const queryClient = new QueryClient();
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,10 @@
+import Alert from "@/assets/alert.svg";
+
 export default function Home() {
-  return <div className="text-xl font-bold">HOME</div>;
+  return (
+    <>
+      <div className="text-xl font-bold">HOME</div>
+      <Alert />
+    </>
+  );
 }
