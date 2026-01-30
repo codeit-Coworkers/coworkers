1f61dd11cd8b3e0f777a24d6f6ae846659b49839
duddj122352-design <duddj122352@gmail.com>
2026-01-30 19:39:00 +0900

    docs: todo 컴포넌트 jsdoc 추가

--- src/components/common/Todo/todo.tsx
+++ src/components/common/Todo/todo.tsx
@@ -2,6 +2,30 @@ import React from "react";
 import CheckedIcon from "@/assets/checkbox-active.svg";
 import UncheckedIcon from "@/assets/checkbox.svg";
 
+/**
+ * @interface TodoProps
+ * @property {string} content - 할 일의 상세 내용입니다.
+ * @property {boolean} isCompleted - 할 일의 완료 여부를 결정합니다. true일 경우 취소선과 opacity-60 스타일이 적용됩니다.
+ * @property {() => void} [onToggle] - 컴포넌트 클릭 시 실행되는 이벤트 핸들러입니다.
+ * @property {boolean} [isWeb=false] - 웹 버전 레이아웃 적용 여부입니다. true일 경우 간격과 아이콘 크기가 확장됩니다.
+ */
+
+/**
+ * Coworkers 프로젝트의 공통 Todo 아이템 컴포넌트입니다.
+ * * @component
+ * @example
+ * ```tsx
+ * <Todo
+ * content="법인 설립 안내 드리기"
+ * isCompleted={false}
+ * onToggle={() => console.log('clicked')}
+ * isWeb={true}
+ * />
+ * ```
+ * * @param {TodoProps} props - Todo 컴포넌트의 속성
+ * @returns {JSX.Element} 모바일/웹 대응이 가능한 Todo 아이템 컴포넌트
+ */
+
 interface TodoProps {
   content: string;
   isCompleted: boolean;
