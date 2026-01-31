acfbe8fb10e65f6adf0be9f14da61ac4459d6aff
summerlane <punkwagon@gmail.com>
2026-01-31 11:38:29 +0900

    feat: 드롭다운 라우트 경로 추가

--- src/app/testRoutes.tsx
+++ src/app/testRoutes.tsx
@@ -3,6 +3,7 @@ import TestGnb from "@/pages/test/TestGnb";
 import TestButton from "@/pages/test/TestButton";
 import TestBadge from "@/pages/test/TestBadge";
 import TestCalendar from "@/pages/test/TestCalendar";
+import TestDropdown from "@/pages/test/TestDropdown";
 
 export const testRoutes = [
   {
@@ -24,5 +25,9 @@ export const testRoutes = [
   {
     path: "/test/calendar",
     element: <TestCalendar />,
+  },
+  {
+    path: "/test/dropdown",
+    element: <TestDropdown />,
   },
 ];
