a1eae9a861dc918cdb71c12de767fcbdba3fb94e
gyo <oey157@nate.com>
2026-02-01 01:52:19 +0900

    fix: 오류 수정

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "@/pages/Home";
 import User from "@/pages/User";
+import Index from "@/pages/Index";
 import { testRoutes } from "./testRoutes";
 
 export const router = createBrowserRouter([
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
   {
     path: "/user/:id",
     element: <User />,
+  },
+  {
+    path: "/index",
+    element: <Index />,
   },
 
   ...testRoutes,
