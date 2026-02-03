6dc496bd651fc4f5dd2b905b23f8565dc4b4cdda
다연집컴 <jewelry0706@naver.com>
2026-02-03 17:48:06 +0900

    fix: testindex li닫기

--- src/pages/test/TestIndex.tsx
+++ src/pages/test/TestIndex.tsx
@@ -58,6 +58,8 @@ export default function TestIndex() {
               className="block rounded-lg border border-gray-200 bg-white px-4 py-3 text-gray-800 hover:border-gray-300 hover:bg-gray-100"
             >
               Input
+            </Link>
+          </li>
           <li>
             <Link
               to="/test/toast"
