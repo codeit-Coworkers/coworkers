7249c5449b724cff7109c6849b857d55e0413f7e
홍요한 <ghddygks45@gmail.com>
2026-02-13 22:48:52 +0900

    fix: gnb 기본프로필이미지, 팀 없을 때 화면 수정

--- src/components/gnb/shared/GnbUserProfile.tsx
+++ src/components/gnb/shared/GnbUserProfile.tsx
@@ -3,6 +3,7 @@ import { useGnbStore } from "../useGnbStore";
 import { useUser } from "@/api/user";
 import { useAuthStore } from "@/stores/useAuthStore";
 import { useNavigate, useParams } from "react-router-dom";
+import UserDefaultProfile from "@/assets/user.svg";
 
 export default function GnbUserProfile() {
   const { data: user } = useUser();
@@ -29,6 +30,7 @@ export default function GnbUserProfile() {
   return (
     <>
       <div>
+        {/* 모바일 */}
         <div className="relative md:hidden">
           <Dropdown
             optionsKey="myHistory"
@@ -59,16 +61,25 @@ export default function GnbUserProfile() {
             keepSelected={false}
             listClassName="right-0"
             icon={
-              <span className="block h-[28px] w-[28px] flex-shrink-0 overflow-hidden rounded-full">
-                <img
-                  src={user.image}
-                  alt={`${user.nickname} 프로필 이미지`}
-                  className="h-full w-full object-cover"
-                />
-              </span>
+              <>
+                <span className="block h-[28px] w-[28px] flex-shrink-0 overflow-hidden rounded-full">
+                  {user.image && (
+                    <img
+                      src={user.image}
+                      alt={`${user.nickname} 프로필 이미지`}
+                      className="h-full w-full object-cover"
+                    />
+                  )}
+                  {!user.image && (
+                    <UserDefaultProfile className="h-full w-full object-cover" />
+                  )}
+                </span>
+              </>
             }
           />
         </div>
+
+        {/* 데스크탑 */}
         <div className="relative hidden md:block">
           <Dropdown
             optionsKey="myHistory"
@@ -105,11 +116,16 @@ export default function GnbUserProfile() {
                 <div
                   className={`flex-shrink-0 ${isFolded ? "h-8 w-8" : "h-10 w-10"}`}
                 >
-                  <img
-                    src={user.image}
-                    alt={`${user.nickname} 프로필 이미지`}
-                    className="h-full w-full rounded-[12px] object-cover"
-                  />
+                  {user.image && (
+                    <img
+                      src={user.image}
+                      alt={`${user.nickname} 프로필 이미지`}
+                      className="h-full w-full rounded-[12px] object-cover"
+                    />
+                  )}
+                  {!user.image && (
+                    <UserDefaultProfile className="h-full w-full rounded-[12px] object-cover" />
+                  )}
                 </div>
                 <div
                   className={`min-w-0 flex-1 flex-col gap-1 text-left transition-opacity ${isFolded ? "pointer-events-none scale-0 opacity-0" : "flex scale-100 opacity-100 duration-200"}`}
